84489f01286f91ca3c2e7d421f661f7b76549eb4
SUHONG\topic <topic828@Naver.com>
2024-05-02 23:43:22 +0900

    메타데이터 등록

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,17 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: '1.6 수학과학전문학원',
-  description: '1.6 수학과학 전문학원입니다',
+  description:
+    '울산 남구 옥동 1.6 수학과학전문학원입니다. 공식상호는 일점육수학과학전문학원이라 네이버지도에서는 해당 이름으로 검색해야 합니다.',
+  openGraph: {
+    title: '1.6 수학과학전문학원',
+    description:
+      '울산 남구 옥동 1.6수학과학전문학원(일점육수학과학전문학원)입니다.',
+    images:
+      'https://github.com/suhong99/1.6math/assets/120103909/205269b2-3969-4afd-8477-686a46aa76c8',
+    locale: 'ko_KR',
+    url: 'http://localhost:3000/',
+  },
 };
 
 export default function RootLayout({
