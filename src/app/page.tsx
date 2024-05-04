54ede8e0369820a4dd80a30004cc6027bf91b0fd
SUHONG\topic <topic828@Naver.com>
2024-05-05 01:19:10 +0900

    카드 컴퍼넌트컴파운드 패턴으로 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import CardList from '@/feature/main/components/CardList';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between lg:p-24">
       <div className="w-full max-w-5xl items-center justify-between font- lg:flex">
-        <h1 className="static top-0 left-0 flex  w-full items-end justify-center bg-gradient-to-t  lg:static lg:size-auto lg:bg-none font-extrabold text-3xl m-4">
+        <h1 className="static top-0 left-0 flex  w-full items-end justify-center bg-gradient-to-t lg:size-auto lg:bg-none font-extrabold text-3xl m-4">
           1.6 수학과학 전문학원
         </h1>
       </div>
@@ -19,71 +20,7 @@ export default function Home() {
           priority
         />
       </div>
-
-      <div className="mb-16 grid text-center lg:text-left lg:w-full lg:mb-0  lg:max-w-5xl md:grid-cols-2 lg:grid-cols-4  m-3">
-        <a
-          href="https://map.naver.com/p/entry/place/1426094200?c=15.00,0,0,0,dh"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 "
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            오시는 길
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <address className="m-0 max-w-[30ch] text-sm opacity-50">
-            울산 남구 문수로335번길 6 <br /> 길상 빌딩 5층
-          </address>
-
-          <p className="m-0 max-w-[30ch] text-xs opacity-50">
-            클릭 시 네이버지도로 이동
-          </p>
-        </a>
-        <div
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          role="button"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            연락처
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-lg opacity-85">052-261-5515</p>
-        </div>
-
-        <div
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          role="button"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            수학 원장
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-lg opacity-85">010-4554-1172</p>
-          <p className="m-0 max-w-[30ch] text-xs opacity-50">김진천 원장 </p>
-        </div>
-        <div
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          role="button"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            과학 원장
-            <span className="inline-block transiion-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-lg opacity-85">010-4554-1172</p>
-          <p className="m-0 max-w-[30ch] text-xs opacity-50">신규하 원장 </p>
-        </div>
-      </div>
+      <CardList />
     </main>
   );
 }
