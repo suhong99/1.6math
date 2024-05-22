c10cc8f6f3bdd98c8c44011fe837dd536d2451b6
SUHONG\topic <topic828@Naver.com>
2024-05-22 18:11:32 +0900

    graph를 이용해 json-ld파일 그룹화

--- src/shared/components/JsonLd.tsx
+++ src/shared/components/JsonLd.tsx
@@ -1,45 +1,45 @@
 import Head from 'next/head';
 
 export default function JsonLd() {
-  const orgJsonLd = {
+  const JsonLd = {
     '@context': 'https://schema.org',
-    '@type': 'Organization',
-    name: '1.6수학과학학원',
-    description: '옥동에서 수학과학을 전문적으로 가르치고 있습니다.',
-    address: {
-      '@type': 'PostalAddress',
-      postalCode: '44646',
-      addressRegion: '울산',
-      addressLocality: '남구 옥동',
-      streetAddress: '문수로335번길 6 길상 빌딩 5층',
-    },
-    contactPoint: {
-      '@type': 'ContactPoint',
-      telephone: '+82-52-261-5515',
-      contactType: '상담',
-      areaServed: ['KR'],
-      availableLanguage: ['Korean'],
-    },
-  };
-
-  const FAQJsonLd = {
-    '@context': 'http://schema.org',
-    '@type': 'FAQPage',
-    mainEntity: [
+    '@graph': [
       {
-        '@type': 'Question',
-        name: '학원 상담시간은 언제인가요?',
-        acceptedAnswer: {
-          '@type': 'Answer',
-          text: '오후 2시에서 7시 입니다',
+        '@type': 'Organization',
+        name: '1.6수학과학학원',
+        description: '옥동에서 수학과학을 전문적으로 가르치고 있습니다.',
+        address: {
+          '@type': 'PostalAddress',
+          postalCode: '44646',
+          addressRegion: '울산',
+          addressLocality: '남구 옥동',
+          streetAddress: '문수로335번길 6 길상 빌딩 5층',
+        },
+        contactPoint: {
+          '@type': 'ContactPoint',
+          telephone: '+82-52-261-5515',
+          contactType: '상담',
+          areaServed: ['KR'],
+          availableLanguage: ['Korean'],
         },
       },
       {
-        '@type': 'Question',
-        name: '수강료는 어떻게 되나요?',
-        acceptedAnswer: {
-          '@type': 'Answer',
-          text: '\
+        '@type': 'FAQPage',
+        mainEntity: [
+          {
+            '@type': 'Question',
+            name: '학원 상담시간은 언제인가요?',
+            acceptedAnswer: {
+              '@type': 'Answer',
+              text: '오후 2시에서 7시 입니다',
+            },
+          },
+          {
+            '@type': 'Question',
+            name: '수강료는 어떻게 되나요?',
+            acceptedAnswer: {
+              '@type': 'Answer',
+              text: '\
                 <ul>\
                   <li>초등부 : 1275분 20만원</li>\
                   <li>중등부(1): 1530분 27만원</li>\
@@ -48,7 +48,9 @@ export default function JsonLd() {
                   <li>고등부(2): 1784분 37만원</li>\
                 </ul>\
               ',
-        },
+            },
+          },
+        ],
       },
     ],
   };
@@ -57,11 +59,7 @@ export default function JsonLd() {
     <Head>
       <script
         type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(orgJsonLd) }}
-      />
-      <script
-        type="application/ld+json"
-        dangerouslySetInnerHTML={{ __html: JSON.stringify(FAQJsonLd) }}
+        dangerouslySetInnerHTML={{ __html: JSON.stringify(JsonLd) }}
       />
     </Head>
   );
