dbf6078a3083e1a6bb3ecd9dc2a5e80113c17cea
SUHONG\topic <topic828@Naver.com>
2024-09-01 13:10:08 +0900

    상담번호 메인화면에서 노출

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import ContentWrapper from '@/feature/introduce/components/ContentWrapper';
 import Introduce from '@/feature/introduce/components/Introduce';
+import Number from '@/feature/introduce/components/Number';
 import System from '@/feature/introduce/components/System';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between lg:p-24 lg:pt-16">
+      <Number />
       <ContentWrapper title="학원 소개">
         <Introduce />
       </ContentWrapper>
