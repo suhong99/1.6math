5495af239b391b8350a0fbf76b29d523c80bb01a
SUHONG\topic <topic828@Naver.com>
2024-10-25 22:03:14 +0900

    제보페이지 UI

--- src/app/suggest/page.tsx
+++ src/app/suggest/page.tsx
@@ -1,5 +1,13 @@
 import SuggestForm from '@/feature/suggest/component/SuggestForm';
 
 export default function Suggest() {
-  return <SuggestForm />;
+  return (
+    <main className="space-y-6 p-6 shadow-md rounded-lg mx-auto mt-4 lg:max-w-2xl w-full">
+      <h1 className="text-2xl font-bold text-center text-gray-800">제보하기</h1>
+      <SuggestForm />
+      <p className="text-gray-600 text-center text-sm">
+        학부모님의 소중한 목소리에 귀 기울이겠습니다.
+      </p>
+    </main>
+  );
 }
