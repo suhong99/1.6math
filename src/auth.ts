a286ce2d05a8592221895699f655a9e0f7f3ce00
SUHONG\topic <topic828@Naver.com>
2024-11-24 11:55:53 +0900

    관리자 로그아웃

--- src/auth.ts
+++ src/auth.ts
@@ -1,5 +1,6 @@
 import NextAuth, { NextAuthConfig } from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { checkUser } from './firestore/helper/auth';
 
 export const authConfig = {
   pages: {
@@ -7,16 +8,25 @@ export const authConfig = {
   },
   theme: { logo: 'https://authjs.dev/img/logo-sm.png' },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
-      const isLoggedIn = !!auth?.user;
+    async authorized({ auth, request: { nextUrl } }) {
+      const isLogIn = !!auth?.user;
       const protectedPath = ['/feedback'];
       const isProtected = protectedPath.some((path) =>
         nextUrl.pathname.startsWith(path)
       );
+
       if (isProtected) {
-        if (isLoggedIn) return true;
-        return Response.redirect(new URL('/', nextUrl));
+        if (isLogIn && auth?.user?.email) {
+          //TODO: 권한관리
+          // const isRegistered = await checkUser({ email: auth.user.email });
+          // if (isRegistered) {
+          //   return true;
+          // }
+        }
+
+        return Response.redirect(new URL('/admin', nextUrl)); // 로그인하지 않은 사용자
       }
+
       return true;
     },
   },
